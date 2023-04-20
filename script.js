7e8019e15405be463988317a0bac322b58ffde7a
Brenda Lyamuya <brendaphilbert51@gmail.com>
2023-04-20 16:15:21 +0300

    image container

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+// image container
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+
+
 // Unsplash APi
 
 const count = 10
 const apiKey = "b_8qlEv-8Q1mEHclK8DIwzXJIQLt1_Vi85WC8rJYDu4"
 const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+function displayPhotos(){
+    photosArray.forEach((photo)=>{
+
+        const item = document.createElement('a');
+        item.setAttribute('href',photo.links.html);
+        item.setAttribute('target','_blank');
+    
+        const img = document.createElement('img');
+        img.setAttribute('src',photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('tittle',photo.alt_description);
+    
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+
+}
+
 async function getPhotos(){
     try{
         const response = await fetch(apiURL)
         photosArray = await response.json()
 
-        console.log (photosArray);
+
+        // console.log (photosArray);
+        displayPhotos();
     }catch(error){}
 }
 
