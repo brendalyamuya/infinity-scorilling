dab537ca4b02bea78c05ca1ec0e1403c8f186fb7
Brenda Lyamuya <brendaphilbert51@gmail.com>
2023-04-21 13:02:13 +0300

    using DRY in javascript

--- script.js
+++ script.js
@@ -12,17 +12,34 @@ const count = 10
 const apiKey = "b_8qlEv-8Q1mEHclK8DIwzXJIQLt1_Vi85WC8rJYDu4"
 const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element,attributes){
+    for(const key in attributes){
+        element.setAttribute(key,attributes[key]);
+    }
+}
+
 function displayPhotos(){
     photosArray.forEach((photo)=>{
 
         const item = document.createElement('a');
-        item.setAttribute('href',photo.links.html);
-        item.setAttribute('target','_blank');
+        // item.setAttribute('href',photo.links.html);
+        // item.setAttribute('target','_blank');
+
+        setAttributes(item,{
+            href:photo.links.html,
+            target:'_blank',
+        })
     
         const img = document.createElement('img');
-        img.setAttribute('src',photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('tittle',photo.alt_description);
+        // img.setAttribute('src',photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('tittle',photo.alt_description);
+
+        setAttributes(img,{
+            src:photo.urls.regular,
+            alt:photo.alt_description,
+            tittle:photo.alt_description,
+        })
     
         item.appendChild(img);
         imageContainer.appendChild(item);
